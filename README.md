36700448a9cc52086cbd69413a9df09790bcfe12
Graydon Hoare <graydon@pobox.com>
2023-11-11 16:01:22 -0800

    Checkpoint

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# brevet
-experimental term evaluator
+# Brevet
+Experimental term evaluator
+
+This is a small, incomplete evaluator for the Ei calculus described in:
+
+Oliveira & Tan, ECOOP 2023, Dependent Merges and First-Class Environments
+10.4230/LIPIcs.CVIT.2016.23
+https://i.cs.hku.hk/~bruno/papers/ecoop23.pdf
---
+++ src/lib.rs
@@ -0,0 +1,340 @@
+use arbitrary::Arbitrary;
+use std::rc::Rc;
+
+#[derive(Debug, Clone, PartialOrd, Ord, PartialEq, Eq, Arbitrary)]
+pub struct Label(Rc<String>);
+
+#[derive(Debug, Clone, PartialOrd, Ord, PartialEq, Eq, Arbitrary)]
+pub enum Type {
+    Top,
+    Int,
+    Arr(Rc<Type>, Rc<Type>),
+    And(Rc<Type>, Rc<Type>),
+    Rcd(Label, Rc<Type>),
+}
+
+impl Type {
+    pub fn is_arr(&self) -> bool {
+        if let Type::Arr(_, _) = self {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Arbitrary)]
+pub enum AbstrMode {
+    AbstrLabHide,
+    AbstrGeneral,
+}
+
+#[derive(Debug, Clone, PartialOrd, Ord, PartialEq, Eq, Arbitrary)]
+pub enum Expr {
+    Top,
+    Int(i64),
+    Query,
+    Abstr(AbstrMode, Rc<Expr>),
+    Close { env: Rc<Expr>, expr: Rc<Expr> },
+    Apply(Rc<Expr>, Rc<Expr>),
+    Merge(Rc<Expr>, Rc<Expr>),
+    Annot(Rc<Expr>, Rc<Type>),
+    Label(Label, Rc<Expr>),
+    Fetch(Rc<Expr>, Label),
+}
+
+impl Expr {
+    pub fn merge(&self, other: &Expr) -> Expr {
+        Expr::Merge(Rc::new(self.clone()), Rc::new(other.clone()))
+    }
+    pub fn is_abstr(&self) -> bool {
+        if let Expr::Abstr(_, _) = self {
+            true
+        } else {
+            false
+        }
+    }
+    pub fn is_arr_annot_abstr(&self) -> bool {
+        if let Expr::Annot(expr, ty) = self {
+            ty.is_arr() && expr.is_abstr()
+        } else {
+            false
+        }
+    }
+    pub fn is_closure(&self) -> bool {
+        if let Expr::Close { env, expr } = self {
+            env.is_val() && expr.is_arr_annot_abstr()
+        } else {
+            false
+        }
+    }
+    pub fn is_closure_subtype_of(&self, c: &Type, d: &Type) -> bool {
+        if self.is_closure() {
+            let Expr::Close { env: _, expr } = self else {
+                return false;
+            };
+            let Expr::Annot(_, ty) = &**expr else {
+                return false;
+            };
+            let Type::Arr(a, b) = &**ty else { return false };
+            subty(c, a) && subty(b, d)
+        } else {
+            false
+        }
+    }
+    pub fn is_val(&self) -> bool {
+        match self {
+            Expr::Top | Expr::Int(_) => true,
+            Expr::Merge(a, b) => a.is_val() && b.is_val(),
+            Expr::Label(_, e) => e.is_val(),
+            _ => self.is_closure(),
+        }
+    }
+    pub fn is_rec(&self, lab: &Label) -> bool {
+        if let Expr::Label(el, e) = self {
+            el == lab && e.is_val()
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialOrd, Ord, PartialEq, Eq, Arbitrary)]
+pub enum Error {
+    General(String),
+    NonValEnv,
+    NonValEval,
+    StuckEval,
+    StuckCast,
+}
+
+impl From<String> for Error {
+    fn from(value: String) -> Self {
+        Error::General(value)
+    }
+}
+
+pub fn subty(a: &Type, b: &Type) -> bool {
+    match (a, b) {
+        // S-Z
+        (Type::Int, Type::Int) => true,
+        // S-TOP
+        (_, Type::Top) => true,
+        // S-ARR
+        (Type::Arr(a1, a2), Type::Arr(b1, b2)) => subty(b1, a1) && subty(a2, b2),
+        // S-ANDL
+        // S-ANDR
+        (Type::And(a1, a2), a3) => subty(a1, a3) || subty(a2, a3),
+        // S-AND
+        (a1, Type::And(a2, a3)) => subty(a1, a2) && subty(a1, a3),
+        // S-RCD
+        (Type::Rcd(l1, e1), Type::Rcd(l2, e2)) => l1 == l2 && subty(e1, e2),
+        _ => false,
+    }
+}
+
+pub fn ordinary(x: &Type) -> bool {
+    match x {
+        // O-INT
+        Type::Int => true,
+        // O-ARROW
+        Type::Arr(_, b) => ordinary(b),
+        // O-RCD
+        Type::Rcd(_, b) => ordinary(b),
+        _ => false,
+    }
+}
+
+pub fn toplike(x: &Type) -> bool {
+    match x {
+        // TL-TOP
+        Type::Top => true,
+        // TL-ARR
+        Type::And(a, b) => toplike(a) && toplike(b),
+        // TL-ARR
+        Type::Arr(_, b) => toplike(b),
+        // TL-RCD
+        Type::Rcd(_, b) => toplike(b),
+        _ => false,
+    }
+}
+
+// "Common Ordinary Super Types"
+pub fn cost(a: &Type, b: &Type) -> bool {
+    match (a, b) {
+        // COST-INT
+        (Type::Int, Type::Int) => true,
+        // COST-ANDL
+        // COST-ANDR
+        (Type::And(a, b), c) => cost(a, c) || cost(b, c),
+        // COST-RANDL
+        // COST-RANDR
+        (a, Type::And(b, c)) => cost(a, b) || cost(a, c),
+        // COST-ARR
+        (Type::Arr(_a, b), Type::Arr(_c, d)) => cost(b, d),
+        // COST-RCD
+        (Type::Rcd(_, x), Type::Rcd(_, y)) => cost(x, y),
+        _ => false,
+    }
+}
+
+// Algorithmic disjointness
+pub fn disj(a: &Type, b: &Type) -> bool {
+    !cost(a, b)
+}
+
+// Value generator for top-like types
+pub fn valgen(ty: &Type) -> Result<Expr, Error> {
+    match ty {
+        Type::Top => Ok(Expr::Top),
+        Type::Int => Err(Error::StuckCast),
+        Type::Arr(_, b) => {
+            let expr = Rc::new(valgen(b)?);
+            let abstr_expr = Rc::new(Expr::Abstr(AbstrMode::AbstrGeneral, expr));
+            let env = Rc::new(Expr::Top);
+            let expr = Rc::new(Expr::Annot(abstr_expr, Rc::new(ty.clone())));
+            Ok(Expr::Close { env, expr })
+        }
+        Type::And(a, b) => Ok(Expr::Merge(Rc::new(valgen(a)?), Rc::new(valgen(b)?))),
+        Type::Rcd(lab, ty) => Ok(Expr::Label(lab.clone(), Rc::new(valgen(ty)?))),
+    }
+}
+
+pub fn cast(val: &Expr, ty: &Type) -> Result<Expr, Error> {
+    match (val, ty) {
+        // CASTING-INT
+        (Expr::Int(_), Type::Int) => Ok(val.clone()),
+        // CASTING-TOP
+        (_, Type::Top) => Ok(val.clone()),
+        (Expr::Close { env, expr }, Type::Arr(c, d)) if val.is_closure_subtype_of(c, d) => {
+            let Expr::Annot(e, cloty) = &**expr else {
+                return Err(Error::StuckCast);
+            };
+            let Type::Arr(a, _) = &**cloty else {
+                return Err(Error::StuckCast);
+            };
+            if toplike(d) {
+                // CASTING-ARROWTL
+                valgen(ty)
+            } else {
+                // CASTING-ARROW
+                // TODO: modes
+                let new_ty = Rc::new(Type::Arr(a.clone(), d.clone()));
+                let new_annot_expr = Rc::new(Expr::Annot(e.clone(), new_ty));
+                Ok(Expr::Close {
+                    env: env.clone(),
+                    expr: new_annot_expr,
+                })
+            }
+        }
+        // CASTING-MERGEVL and CASTING-MERGEVL
+        (Expr::Merge(a, b), _) if ordinary(ty) => {
+            if let Ok(c) = cast(a, ty) {
+                Ok(c)
+            } else if let Ok(c) = cast(b, ty) {
+                Ok(c)
+            } else {
+                Err(Error::StuckCast)
+            }
+        }
+        // CASTING-AND
+        (_, Type::And(a, b)) => Ok(Expr::Merge(Rc::new(cast(val, a)?), Rc::new(cast(val, b)?))),
+        // CASTING-RCD
+        (Expr::Label(elab, e), Type::Rcd(tlab, t)) if elab == tlab => {
+            Ok(Expr::Label(elab.clone(), Rc::new(cast(e, t)?)))
+        }
+        _ => Err(Error::StuckCast),
+    }
+}
+
+pub fn step(env: &Expr, expr: &Expr) -> Result<Expr, Error> {
+    if !env.is_val() {
+        return Err(Error::NonValEnv);
+    }
+    if expr.is_val() {
+        return Ok(expr.clone());
+    }
+    match expr {
+        // STEP-CTX
+        Expr::Query => Ok(env.clone()),
+
+        // STEP-ANNOV
+        Expr::Annot(v, t) if v.is_val() => cast(v, t),
+
+        // STEP-MERGER
+        Expr::Merge(v, e) if v.is_val() => {
+            let env_with_v = env.merge(v);
+            let ev = step(&env_with_v, e)?;
+            Ok(v.merge(&ev))
+        }
+
+        // STEP-CLOSURE
+        e if e.is_arr_annot_abstr() => Ok(Expr::Close {
+            env: Rc::new(env.clone()),
+            expr: Rc::new(e.clone()),
+        }),
+
+        // STEP-BOX
+        Expr::Close {
+            env: env1,
+            expr: expr1,
+        } if env1.is_val() && !expr.is_closure() => step(env1, expr1),
+
+        // STEP-BOXV
+        Expr::Close {
+            env: env1,
+            expr: expr1,
+        } if env1.is_val() && expr1.is_val() => Ok((**expr1).clone()),
+
+        // STEP-BETA
+        Expr::Apply(clo, arg) if clo.is_closure() && arg.is_val() => {
+            let Expr::Close {
+                env: env1,
+                expr: expr1,
+            } = &**clo
+            else {
+                return Err(Error::StuckEval);
+            };
+            let Expr::Annot(abstr, ty) = &**expr1 else {
+                return Err(Error::StuckEval);
+            };
+            let Expr::Abstr(_mode, body) = &**abstr else {
+                return Err(Error::StuckEval);
+            };
+            let Type::Arr(_, b) = &**ty else {
+                return Err(Error::StuckEval);
+            };
+            // TODO: modes
+            let new_env = env1.merge(arg);
+            let new_annot = Expr::Annot(body.clone(), b.clone());
+            Ok(Expr::Close {
+                env: Rc::new(new_env),
+                expr: Rc::new(new_annot),
+            })
+        }
+
+        // STEP-PROJV
+        Expr::Fetch(e, lab) if e.is_rec(lab) => {
+            let Expr::Label(_, el) = &**e else {
+                return Err(Error::StuckEval);
+            };
+            Ok((**el).clone())
+        }
+
+        // STEP-EVAL
+        Expr::Annot(e, t) => Ok(Expr::Annot(Rc::new(step(env, e)?), t.clone())),
+        Expr::Merge(a, b) => Ok(Expr::Merge(Rc::new(step(env, a)?), b.clone())),
+        Expr::Label(lab, e) => Ok(Expr::Label(lab.clone(), Rc::new(step(env, e)?))),
+        Expr::Fetch(e, lab) => Ok(Expr::Fetch(Rc::new(step(env, e)?), lab.clone())),
+        Expr::Apply(clo, arg) if !clo.is_val() => {
+            Ok(Expr::Apply(Rc::new(step(env, clo)?), arg.clone()))
+        }
+        Expr::Apply(clo, arg) => Ok(Expr::Apply(clo.clone(), Rc::new(step(env, arg)?))),
+        Expr::Close { env: env1, expr } => Ok(Expr::Close {
+            env: Rc::new(step(env, env1)?),
+            expr: expr.clone(),
+        }),
+
+        _ => Err(Error::StuckEval),
+    }
+}
