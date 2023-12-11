bbc183bf2870cd0435a1a0ca0d4bd9a9fcf15bda
Graydon Hoare <graydon@pobox.com>
2023-12-10 22:52:19 -0800

    parsing

--- src/arena.rs
+++ src/arena.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Display, Formatter};
 
 use bumpalo::Bump;
-use yap::{one_of, types::WithContext, IntoTokens, Tokens};
+use yap::{one_of, types::WithContext, IntoTokens, TokenLocation, Tokens};
 
 use crate::AbstrMode;
 
@@ -35,51 +35,55 @@ pub enum Expr<'a> {
 }
 
 fn maybe_parse_expr_binop<'a>(
-    lhs: &'a Expr<'a>,
+    mut lhs: &'a Expr<'a>,
     t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>,
 ) -> Option<&'a Expr<'a>> {
     let bump: &Bump = *t.context();
     fn can_begin_expr(c: char) -> bool {
         c.is_ascii_digit() || c == '{' || c == '?' || c == '^'
     }
-    match t.peek() {
-        None => Some(lhs),
-        Some(x) => match x {
-            '>' => {
-                t.next();
-                let rhs = parse_expr(t)?;
-                Some(&*bump.alloc(Expr::Close {
-                    env: lhs,
-                    expr: rhs,
-                }))
-            }
-            ',' => {
-                t.next();
-                let rhs = parse_expr(t)?;
-                Some(&*bump.alloc(Expr::Merge(lhs, rhs)))
-            }
-            ':' => {
-                t.next();
-                let ty = parse_type(t)?;
-                Some(&*bump.alloc(Expr::Annot(lhs, ty)))
-            }
-            '.' => {
-                t.next();
-                let l = parse_label(t)?;
-                Some(&*bump.alloc(Expr::Fetch(lhs, l)))
-            }
-            x if can_begin_expr(x) => {
-                let rhs = parse_expr(t)?;
-                Some(&*bump.alloc(Expr::Apply(lhs, rhs)))
-            }
-            _ => Some(lhs),
-        },
+    loop {
+        t.skip_while(|c| c.is_ascii_whitespace());
+        match t.peek() {
+            None => return Some(lhs),
+            Some(x) => match x {
+                '>' => {
+                    t.next();
+                    let rhs = parse_expr(t)?;
+                    lhs = &*bump.alloc(Expr::Close {
+                        env: lhs,
+                        expr: rhs,
+                    })
+                }
+                ',' => {
+                    t.next();
+                    let rhs = parse_expr(t)?;
+                    lhs = &*bump.alloc(Expr::Merge(lhs, rhs))
+                }
+                ':' => {
+                    t.next();
+                    let ty = parse_type(t)?;
+                    lhs = &*bump.alloc(Expr::Annot(lhs, ty))
+                }
+                '.' => {
+                    t.next();
+                    let l = parse_label(t)?;
+                    lhs = &*bump.alloc(Expr::Fetch(lhs, l))
+                }
+                x if can_begin_expr(x) => {
+                    let rhs = parse_expr(t)?;
+                    lhs = &*bump.alloc(Expr::Apply(lhs, rhs))
+                }
+                _ => return Some(lhs),
+            },
+        }
     }
 }
 
 pub fn parse_expr<'a>(
     t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>,
 ) -> Option<&'a Expr<'a>> {
+    t.skip_while(|c| c.is_ascii_whitespace());
     let e = one_of!(t;
         parse_int(t),
         parse_top(t),
@@ -94,18 +98,24 @@ fn parse_braced_expr<'a>(
     t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>,
 ) -> Option<&'a Expr<'a>> {
     let bump = *t.context();
+    t.skip_while(|c| c.is_ascii_whitespace());
     t.token('{').then_some(())?;
+    t.skip_while(|c| c.is_ascii_whitespace());
     match t.peek()? {
         x if x.is_ascii_alphabetic() => {
             let l = parse_label(t)?;
+            t.skip_while(|c| c.is_ascii_whitespace());
             t.token('=').then_some(())?;
             let e = parse_expr(t)?;
+            t.skip_while(|c| c.is_ascii_whitespace());
             t.token('}').then_some(())?;
             Some(&*bump.alloc(Expr::Label(l, e)))
         }
         _ => {
             let e = parse_expr(t)?;
+            t.skip_while(|c| c.is_ascii_whitespace());
             t.token('}').then_some(())?;
+            t.skip_while(|c| c.is_ascii_whitespace());
             let mode = one_of!(t;
                 t.token('@').then(|| AbstrMode::AbstrGeneral),
                 t.token('*').then(|| AbstrMode::AbstrLabHide),
@@ -117,6 +127,7 @@ fn parse_braced_expr<'a>(
 
 fn parse_top<'a>(t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>) -> Option<&'a Expr<'a>> {
     let bump: &Bump = *t.context();
+    t.skip_while(|c| c.is_ascii_whitespace());
     t.token('^').then(|| &*bump.alloc(Expr::Top))
 }
 
@@ -124,6 +135,7 @@ fn parse_int<'a>(t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>) -> Opt
     let bump: &Bump = *t.context();
     let mut i = 0;
     let mut found_digits = false;
+    t.skip_while(|c| c.is_ascii_whitespace());
     loop {
         match t.peek() {
             Some(c) if c.is_digit(10) => {
@@ -145,6 +157,7 @@ fn parse_int<'a>(t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>) -> Opt
 
 fn parse_label<'a>(t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>) -> Option<Label<'a>> {
     let bump: &Bump = *t.context();
+    t.skip_while(|c| c.is_ascii_whitespace());
     let s = bumpalo::collections::String::from_iter_in(
         t.take_while(|c| c.is_ascii_alphabetic()).as_iter(),
         bump,
@@ -161,10 +174,12 @@ fn parse_query<'a>(
     t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>,
 ) -> Option<&'a Expr<'a>> {
     let bump: &Bump = *t.context();
+    t.skip_while(|c| c.is_ascii_whitespace());
     t.token('?').then(|| &*bump.alloc(Expr::Query))
 }
 
 fn parse_type<'a>(t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>) -> Option<&'a Type<'a>> {
+    t.skip_while(|c| c.is_ascii_whitespace());
     let ty = one_of!(t;
         parse_top_type(t),
         parse_int_type(t),
@@ -177,6 +192,7 @@ fn parse_top_type<'a>(
     t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>,
 ) -> Option<&'a Type<'a>> {
     let bump: &Bump = *t.context();
+    t.skip_while(|c| c.is_ascii_whitespace());
     t.token('^').then(|| &*bump.alloc(Type::Top))
 }
 
@@ -184,41 +200,48 @@ fn parse_int_type<'a>(
     t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>,
 ) -> Option<&'a Type<'a>> {
     let bump: &Bump = *t.context();
+    t.skip_while(|c| c.is_ascii_whitespace());
     t.tokens("Int".chars()).then(|| &*bump.alloc(Type::Int))
 }
 
 fn maybe_parse_ty_binop<'a>(
-    lhs: &'a Type<'a>,
+    mut lhs: &'a Type<'a>,
     t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>,
 ) -> Option<&'a Type<'a>> {
     let bump: &Bump = *t.context();
-    match t.peek() {
-        None => Some(lhs),
-        Some(x) => match x {
-            '&' => {
-                t.next();
-                let rhs = parse_type(t)?;
-                Some(&*bump.alloc(Type::And(lhs, rhs)))
-            }
-            '-' => {
-                t.next();
-                t.token('>').then_some(())?;
-                let rhs = parse_type(t)?;
-                Some(&*bump.alloc(Type::Arr(lhs, rhs)))
-            }
-            _ => Some(lhs),
-        },
+    loop {
+        t.skip_while(|c| c.is_ascii_whitespace());
+        match t.peek() {
+            None => return Some(lhs),
+            Some(x) => match x {
+                '&' => {
+                    t.next();
+                    let rhs = parse_type(t)?;
+                    lhs = &*bump.alloc(Type::And(lhs, rhs));
+                }
+                '-' => {
+                    t.next();
+                    t.token('>').then_some(())?;
+                    let rhs = parse_type(t)?;
+                    lhs = &*bump.alloc(Type::Arr(lhs, rhs));
+                }
+                _ => return Some(lhs),
+            },
+        }
     }
 }
 
 fn parse_rcd_type<'a>(
     t: &mut WithContext<impl Tokens<Item = char>, &'a Bump>,
 ) -> Option<&'a Type<'a>> {
+    t.skip_while(|c| c.is_ascii_whitespace());
     let bump: &Bump = *t.context();
     t.token('{').then_some(())?;
     let l = parse_label(t)?;
+    t.skip_while(|c| c.is_ascii_whitespace());
     t.token(':').then_some(())?;
     let ty = parse_type(t)?;
+    t.skip_while(|c| c.is_ascii_whitespace());
     t.token('}').then_some(())?;
     Some(&*bump.alloc(Type::Rcd(l, ty)))
 }
@@ -234,7 +257,7 @@ impl Display for Type<'_> {
         match self {
             Type::Top => write!(f, "^"),
             Type::Int => write!(f, "Int"),
-            Type::Arr(ty1, ty2) => write!(f, "{}->{}", ty1, ty2),
+            Type::Arr(ty1, ty2) => write!(f, "{} -> {}", ty1, ty2),
             Type::And(ty1, ty2) => write!(f, "{}&{}", ty1, ty2),
             Type::Rcd(l, ty) => write!(f, "{{{}:{}}}", l, ty),
         }
@@ -248,10 +271,10 @@ impl Display for Expr<'_> {
             Expr::Int(i) => write!(f, "{}", i),
             Expr::Query => write!(f, "?"),
             Expr::Abstr(mode, e) => write!(f, "{{{}}}{}", e, mode),
-            Expr::Close { env, expr } => write!(f, "{}>{}", env, expr),
-            Expr::Apply(e1, e2) => write!(f, "{}{}", e1, e2),
+            Expr::Close { env, expr } => write!(f, "{} > {}", env, expr),
+            Expr::Apply(e1, e2) => write!(f, "{} {}", e1, e2),
             Expr::Merge(e1, e2) => write!(f, "{},{}", e1, e2),
-            Expr::Annot(e, ty) => write!(f, "{}:{}", e, ty),
+            Expr::Annot(e, ty) => write!(f, "{} : {}", e, ty),
             Expr::Label(l, e) => write!(f, "{{{}={}}}", l, e),
             Expr::Fetch(e, l) => write!(f, "{}.{}", e, l),
         }
@@ -268,19 +291,41 @@ impl Display for AbstrMode {
 }
 
 #[test]
-fn test_parse() {
+fn test_parse() -> Result<(), String> {
     let bump = Bump::new();
 
-    fn test_round_trip<'a>(bump: &'a Bump, s: &'a str) {
+    fn test_round_trip<'a>(bump: &'a Bump, s: &'a str) -> Result<(), String> {
         println!("testing: {}", s);
         let mut t = s.into_tokens().with_context(bump);
-        let e = parse_expr(&mut t).unwrap();
-        println!("parsed: {:?}", e);
-        println!("format: {}", e);
-        assert_eq!(s, format!("{}", e));
+        match parse_expr(&mut t) {
+            Some(e) => {
+                if t.eof() {
+                    println!("parsed: {:?}", e);
+                    println!("format: {}", e);
+                    assert_eq!(s, format!("{}", e));
+                    Ok(())
+                } else {
+                    let loc = t.location().offset();
+                    Err(format!("partial parse at '{}'", &s[loc..]))
+                }
+            }
+            None => {
+                let loc = t.location().offset();
+                Err(format!("failed to parse at '{}'", &s[loc..]))
+            }
+        }
     }
 
-    for s in ["1", "?", "?.x", "{x=1}", "{x=1}.x"] {
-        test_round_trip(&bump, s);
+    for s in [
+        "1",
+        "?",
+        "?.x",
+        "{x=1}",
+        "{x=1}.x",
+        "{x=1}.x : ^",
+        "{x=1},{y=2} ?.x ?.y ?.z : Int&Int -> Int",
+    ] {
+        test_round_trip(&bump, s)?;
     }
+    Ok(())
 }
